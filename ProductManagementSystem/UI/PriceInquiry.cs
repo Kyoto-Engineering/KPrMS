bde928b4f7e40739a7ddd1debdb20abcb977641c
Ikramunnesa <ikraafi@gmail.com>
2017-07-03 11:40:08 +0600

    price inquiry ui added

---
+++ ProductManagementSystem/UI/PriceInquiry.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ProductManagementSystem.UI
+{
+    public partial class PriceInquiry : Form
+    {
+        public PriceInquiry()
+        {
+            InitializeComponent();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
