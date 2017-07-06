a0a416cc82afcc941f222a71170ed38192235e32
Ikramunnesa <ikraafi@gmail.com>
2017-07-06 20:02:01 +0600

    new report viewer added

---
+++ ProductManagementSystem/Reports/ReportByBrand.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ProductManagementSystem.Reports
+{
+    public partial class ReportByBrand : Form
+    {
+        private SqlConnection con;
+        private SqlCommand cmd;
+        private SqlDataReader rdr;
+        ConnectionString cs = new ConnectionString();
+        private int BrandId;
+        public ReportByBrand()
+        {
+            InitializeComponent();
+        }
+
+        private void GetButton_Click(object sender, EventArgs e)
+        {
+            GetButton.Enabled = false;
+
+
+            if (!string.IsNullOrWhiteSpace(BrandIdComboBox.Text))
+            {
+
+                if (PorListRadioButton.Checked)
+                {
+                    GetProductList();
+                    Clear();
+                }
+                else
+                {
+                    MessageBox.Show(@"Please Select a Brand Id");
+                }
+
+                GetButton.Enabled = true;
+            }
+        }
+
+        private void Clear()
+        {
+            BrandIdComboBox.SelectedIndex = -1;
+            ProListRadioButton.Checked = false;
+        }
+        private GetProductList()
+        {
+            ParameterField paramField1 = new ParameterField();
+
+
+            //creating an object of ParameterFields class
+            ParameterFields paramFields1 = new ParameterFields();
+
+            //creating an object of ParameterDiscreteValue class
+            ParameterDiscreteValue paramDiscreteValue1 = new ParameterDiscreteValue();
+
+            //set the parameter field name
+            paramField1.Name = "BrandId";
+
+            //set the parameter value
+            paramDiscreteValue1.Value = BrandIdCombobox.Text;
+
+            //add the parameter value in the ParameterField object
+            paramField1.CurrentValues.Add(paramDiscreteValue1);
+
+            //add the parameter in the ParameterFields object
+            paramFields1.Add(paramField1);
+            ReportViewer f2 = new ReportViewer();
+            TableLogOnInfos reportLogonInfos = new TableLogOnInfos();
+            TableLogOnInfo reportLogonInfo = new TableLogOnInfo();
+            ConnectionInfo reportConInfo = new ConnectionInfo();
+            Tables tables = default(Tables);
+            //	Table table = default(Table);
+            var with1 = reportConInfo;
+            with1.ServerName = "tcp:KyotoServer,49172";
+            with1.DatabaseName = "ProductNRelatedDB";
+            with1.UserID = "sa";
+            with1.Password = "SystemAdministrator";
+            ReportByBrand cr = new ReportByBrand();
+            tables = cr.Database.Tables;
+            foreach (Table table in tables)
+            {
+                reportLogonInfo = table.LogOnInfo;
+                reportLogonInfo.ConnectionInfo = reportConInfo;
+                table.ApplyLogOnInfo(reportLogonInfo);
+            }
+
+            f2.crystalReportViewer1.ParameterFieldInfo = paramFields1;
+            f2.crystalReportViewer1.ReportSource = cr;
+            this.Visible = false;
+
+            f2.ShowDialog();
+            this.Visible = true;
+        }
+              
+  
+    }
+}
