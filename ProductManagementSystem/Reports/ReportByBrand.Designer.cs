e5bf568d8d113dd55bd295463c74b445559110e3
Ikramunnesa <ikraafi@gmail.com>
2017-07-08 16:35:10 +0600

    parameter name change

--- ProductManagementSystem/Reports/ReportByBrand.Designer.cs
+++ ProductManagementSystem/Reports/ReportByBrand.Designer.cs
@@ -29,9 +29,8 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.BrandIdComboBox = new System.Windows.Forms.ComboBox();
+            this.BrandNameComboBox = new System.Windows.Forms.ComboBox();
             this.GetButton = new System.Windows.Forms.Button();
-            this.ProListRadioButton = new System.Windows.Forms.RadioButton();
             this.SuspendLayout();
             // 
             // label1
@@ -40,17 +39,17 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(43, 26);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(124, 17);
+            this.label1.Size = new System.Drawing.Size(127, 17);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Brand Id          :";
+            this.label1.Text = "Brand Name     :";
             // 
-            // BrandIdComboBox
+            // BrandNameComboBox
             // 
-            this.BrandIdComboBox.FormattingEnabled = true;
-            this.BrandIdComboBox.Location = new System.Drawing.Point(168, 25);
-            this.BrandIdComboBox.Name = "BrandIdComboBox";
-            this.BrandIdComboBox.Size = new System.Drawing.Size(121, 21);
-            this.BrandIdComboBox.TabIndex = 1;
+            this.BrandNameComboBox.FormattingEnabled = true;
+            this.BrandNameComboBox.Location = new System.Drawing.Point(168, 25);
+            this.BrandNameComboBox.Name = "BrandNameComboBox";
+            this.BrandNameComboBox.Size = new System.Drawing.Size(121, 21);
+            this.BrandNameComboBox.TabIndex = 1;
             // 
             // GetButton
             // 
@@ -63,25 +62,13 @@
             this.GetButton.UseVisualStyleBackColor = true;
             this.GetButton.Click += new System.EventHandler(this.GetButton_Click);
             // 
-            // ProListRadioButton
-            // 
-            this.ProListRadioButton.AutoSize = true;
-            this.ProListRadioButton.Location = new System.Drawing.Point(168, 72);
-            this.ProListRadioButton.Name = "ProListRadioButton";
-            this.ProListRadioButton.Size = new System.Drawing.Size(81, 17);
-            this.ProListRadioButton.TabIndex = 3;
-            this.ProListRadioButton.TabStop = true;
-            this.ProListRadioButton.Text = "Product List";
-            this.ProListRadioButton.UseVisualStyleBackColor = true;
-            // 
             // ReportByBrand
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(395, 231);
-            this.Controls.Add(this.ProListRadioButton);
             this.Controls.Add(this.GetButton);
-            this.Controls.Add(this.BrandIdComboBox);
+            this.Controls.Add(this.BrandNameComboBox);
             this.Controls.Add(this.label1);
             this.Name = "ReportByBrand";
             this.Text = "Report By Brand";
@@ -94,8 +81,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox BrandIdComboBox;
+        private System.Windows.Forms.ComboBox BrandNameComboBox;
         private System.Windows.Forms.Button GetButton;
-        private System.Windows.Forms.RadioButton ProListRadioButton;
     }
 }
