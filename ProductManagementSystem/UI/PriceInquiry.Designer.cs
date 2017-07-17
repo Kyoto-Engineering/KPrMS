4cc775b61d0577b17c58d0df175d22b7942d0586
Ikramunnesa <ikraafi@gmail.com>
2017-07-17 20:35:35 +0600

    Country of origin combobox, >> button added and price text box deleted

--- ProductManagementSystem/UI/PriceInquiry.Designer.cs
+++ ProductManagementSystem/UI/PriceInquiry.Designer.cs
@@ -29,14 +29,15 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.PriceTextBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.COOComboBox = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.QtyTextBox = new System.Windows.Forms.TextBox();
             this.RemarksTextBox = new System.Windows.Forms.TextBox();
             this.InqFromTextBox = new System.Windows.Forms.TextBox();
             this.ProCodeTextBox = new System.Windows.Forms.TextBox();
             this.ProDesTextBox = new System.Windows.Forms.TextBox();
             this.ModelTextBox = new System.Windows.Forms.TextBox();
-            this.label7 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
@@ -51,14 +52,15 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.PriceTextBox);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.COOComboBox);
+            this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Controls.Add(this.QtyTextBox);
             this.groupBox1.Controls.Add(this.RemarksTextBox);
             this.groupBox1.Controls.Add(this.InqFromTextBox);
             this.groupBox1.Controls.Add(this.ProCodeTextBox);
             this.groupBox1.Controls.Add(this.ProDesTextBox);
             this.groupBox1.Controls.Add(this.ModelTextBox);
-            this.groupBox1.Controls.Add(this.label7);
             this.groupBox1.Controls.Add(this.label6);
             this.groupBox1.Controls.Add(this.label5);
             this.groupBox1.Controls.Add(this.label4);
@@ -68,24 +70,45 @@
             this.groupBox1.Controls.Add(this.SaveButton);
             this.groupBox1.Location = new System.Drawing.Point(25, 69);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(513, 397);
+            this.groupBox1.Size = new System.Drawing.Size(551, 397);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             // 
-            // PriceTextBox
+            // label7
             // 
-            this.PriceTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.PriceTextBox.Location = new System.Drawing.Point(193, 275);
-            this.PriceTextBox.Name = "PriceTextBox";
-            this.PriceTextBox.Size = new System.Drawing.Size(249, 23);
-            this.PriceTextBox.TabIndex = 7;
-            this.PriceTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.PriceTextBox_KeyDown);
-            this.PriceTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.PriceTextBox_KeyPress);
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(18, 157);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(172, 17);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Country of Origin       :";
+            // 
+            // COOComboBox
+            // 
+            this.COOComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.COOComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.COOComboBox.FormattingEnabled = true;
+            this.COOComboBox.Location = new System.Drawing.Point(193, 157);
+            this.COOComboBox.Name = "COOComboBox";
+            this.COOComboBox.Size = new System.Drawing.Size(249, 25);
+            this.COOComboBox.TabIndex = 10;
+            this.COOComboBox.SelectedIndexChanged += new System.EventHandler(this.COOComboBox_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(458, 192);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = ">>";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // QtyTextBox
             // 
             this.QtyTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.QtyTextBox.Location = new System.Drawing.Point(193, 239);
+            this.QtyTextBox.Location = new System.Drawing.Point(193, 271);
             this.QtyTextBox.Name = "QtyTextBox";
             this.QtyTextBox.Size = new System.Drawing.Size(249, 23);
             this.QtyTextBox.TabIndex = 6;
@@ -95,7 +118,7 @@
             // RemarksTextBox
             // 
             this.RemarksTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.RemarksTextBox.Location = new System.Drawing.Point(193, 196);
+            this.RemarksTextBox.Location = new System.Drawing.Point(193, 224);
             this.RemarksTextBox.MaxLength = 255;
             this.RemarksTextBox.Multiline = true;
             this.RemarksTextBox.Name = "RemarksTextBox";
@@ -106,7 +129,7 @@
             // InqFromTextBox
             // 
             this.InqFromTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.InqFromTextBox.Location = new System.Drawing.Point(193, 166);
+            this.InqFromTextBox.Location = new System.Drawing.Point(193, 192);
             this.InqFromTextBox.MaxLength = 255;
             this.InqFromTextBox.Name = "InqFromTextBox";
             this.InqFromTextBox.Size = new System.Drawing.Size(249, 23);
@@ -116,7 +139,7 @@
             // ProCodeTextBox
             // 
             this.ProCodeTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ProCodeTextBox.Location = new System.Drawing.Point(193, 134);
+            this.ProCodeTextBox.Location = new System.Drawing.Point(193, 121);
             this.ProCodeTextBox.MaxLength = 255;
             this.ProCodeTextBox.Name = "ProCodeTextBox";
             this.ProCodeTextBox.Size = new System.Drawing.Size(249, 23);
@@ -130,7 +153,7 @@
             this.ProDesTextBox.MaxLength = 255;
             this.ProDesTextBox.Multiline = true;
             this.ProDesTextBox.Name = "ProDesTextBox";
-            this.ProDesTextBox.Size = new System.Drawing.Size(249, 66);
+            this.ProDesTextBox.Size = new System.Drawing.Size(249, 47);
             this.ProDesTextBox.TabIndex = 2;
             this.ProDesTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.ProDesTextBox_KeyDown);
             // 
@@ -144,21 +167,11 @@
             this.ModelTextBox.TabIndex = 1;
             this.ModelTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.ModelTextBox_KeyDown);
             // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(21, 278);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(175, 17);
-            this.label7.TabIndex = 7;
-            this.label7.Text = "Price                         :";
-            // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(21, 242);
+            this.label6.Location = new System.Drawing.Point(21, 274);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(175, 17);
             this.label6.TabIndex = 6;
@@ -168,7 +181,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(21, 205);
+            this.label5.Location = new System.Drawing.Point(21, 233);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(176, 17);
             this.label5.TabIndex = 5;
@@ -179,7 +192,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(21, 166);
+            this.label4.Location = new System.Drawing.Point(21, 192);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(173, 17);
             this.label4.TabIndex = 4;
@@ -189,7 +202,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(18, 137);
+            this.label3.Location = new System.Drawing.Point(18, 124);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(176, 17);
             this.label3.TabIndex = 3;
@@ -219,7 +232,7 @@
             // 
             this.SaveButton.BackColor = System.Drawing.Color.MediumSlateBlue;
             this.SaveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.SaveButton.Location = new System.Drawing.Point(264, 335);
+            this.SaveButton.Location = new System.Drawing.Point(263, 328);
             this.SaveButton.Name = "SaveButton";
             this.SaveButton.Size = new System.Drawing.Size(100, 54);
             this.SaveButton.TabIndex = 8;
@@ -262,6 +275,7 @@
             this.Controls.Add(this.groupBox1);
             this.Name = "PriceInquiry";
             this.Text = "Price Inquiry";
+            this.Load += new System.EventHandler(this.PriceInquiry_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
@@ -272,14 +286,12 @@
         #endregion
 
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.TextBox PriceTextBox;
         private System.Windows.Forms.TextBox QtyTextBox;
         private System.Windows.Forms.TextBox RemarksTextBox;
         private System.Windows.Forms.TextBox InqFromTextBox;
         private System.Windows.Forms.TextBox ProCodeTextBox;
         private System.Windows.Forms.TextBox ProDesTextBox;
         private System.Windows.Forms.TextBox ModelTextBox;
-        private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
@@ -289,5 +301,8 @@
         private System.Windows.Forms.Button SaveButton;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Button CloseButton;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox COOComboBox;
+        private System.Windows.Forms.Button button1;
     }
 }
