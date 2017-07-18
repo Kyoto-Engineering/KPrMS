c812c31e4b3d0bca709b59753e3e8c0c589a65dd
Ikramunnesa <ikraafi@gmail.com>
2017-07-18 12:51:08 +0600

    design modified

--- ProductManagementSystem/UI/PriceInquiry.Designer.cs
+++ ProductManagementSystem/UI/PriceInquiry.Designer.cs
@@ -31,7 +31,7 @@
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.label7 = new System.Windows.Forms.Label();
             this.COOComboBox = new System.Windows.Forms.ComboBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.InqFromButton = new System.Windows.Forms.Button();
             this.QtyTextBox = new System.Windows.Forms.TextBox();
             this.RemarksTextBox = new System.Windows.Forms.TextBox();
             this.InqFromTextBox = new System.Windows.Forms.TextBox();
@@ -54,7 +54,7 @@
             // 
             this.groupBox1.Controls.Add(this.label7);
             this.groupBox1.Controls.Add(this.COOComboBox);
-            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.InqFromButton);
             this.groupBox1.Controls.Add(this.QtyTextBox);
             this.groupBox1.Controls.Add(this.RemarksTextBox);
             this.groupBox1.Controls.Add(this.InqFromTextBox);
@@ -95,15 +95,15 @@
             this.COOComboBox.TabIndex = 10;
             this.COOComboBox.SelectedIndexChanged += new System.EventHandler(this.COOComboBox_SelectedIndexChanged);
             // 
-            // button1
+            // InqFromButton
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(458, 192);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 9;
-            this.button1.Text = ">>";
-            this.button1.UseVisualStyleBackColor = true;
+            this.InqFromButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InqFromButton.Location = new System.Drawing.Point(458, 192);
+            this.InqFromButton.Name = "InqFromButton";
+            this.InqFromButton.Size = new System.Drawing.Size(50, 23);
+            this.InqFromButton.TabIndex = 9;
+            this.InqFromButton.Text = ">>";
+            this.InqFromButton.UseVisualStyleBackColor = true;
             // 
             // QtyTextBox
             // 
@@ -303,6 +303,6 @@
         private System.Windows.Forms.Button CloseButton;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.ComboBox COOComboBox;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button InqFromButton;
     }
 }
