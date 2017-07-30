717c15a9af996f2c78f53ecc29b904268beb2a27
Shimul <shimul39ju@gmail.com>
2017-07-30 21:30:06 +0600

    Fixed the Inquiry UI

--- ProductManagementSystem/UI/PriceInquiry.Designer.cs
+++ ProductManagementSystem/UI/PriceInquiry.Designer.cs
@@ -41,8 +41,18 @@
             this.label1 = new System.Windows.Forms.Label();
             this.SaveButton = new System.Windows.Forms.Button();
             this.label8 = new System.Windows.Forms.Label();
-            this.CloseButton = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -58,16 +68,16 @@
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.SaveButton);
-            this.groupBox1.Location = new System.Drawing.Point(25, 69);
+            this.groupBox1.Location = new System.Drawing.Point(40, 131);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(498, 335);
+            this.groupBox1.Size = new System.Drawing.Size(489, 285);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             // 
             // QtyTextBox
             // 
             this.QtyTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.QtyTextBox.Location = new System.Drawing.Point(193, 203);
+            this.QtyTextBox.Location = new System.Drawing.Point(193, 199);
             this.QtyTextBox.Name = "QtyTextBox";
             this.QtyTextBox.Size = new System.Drawing.Size(249, 23);
             this.QtyTextBox.TabIndex = 5;
@@ -77,7 +87,7 @@
             // RemarksTextBox
             // 
             this.RemarksTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.RemarksTextBox.Location = new System.Drawing.Point(193, 156);
+            this.RemarksTextBox.Location = new System.Drawing.Point(193, 152);
             this.RemarksTextBox.MaxLength = 255;
             this.RemarksTextBox.Multiline = true;
             this.RemarksTextBox.Name = "RemarksTextBox";
@@ -88,7 +98,7 @@
             // InqFromTextBox
             // 
             this.InqFromTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.InqFromTextBox.Location = new System.Drawing.Point(193, 122);
+            this.InqFromTextBox.Location = new System.Drawing.Point(193, 118);
             this.InqFromTextBox.MaxLength = 255;
             this.InqFromTextBox.Name = "InqFromTextBox";
             this.InqFromTextBox.Size = new System.Drawing.Size(249, 23);
@@ -98,7 +108,7 @@
             // ProDesTextBox
             // 
             this.ProDesTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ProDesTextBox.Location = new System.Drawing.Point(193, 62);
+            this.ProDesTextBox.Location = new System.Drawing.Point(193, 58);
             this.ProDesTextBox.MaxLength = 255;
             this.ProDesTextBox.Multiline = true;
             this.ProDesTextBox.Name = "ProDesTextBox";
@@ -109,7 +119,7 @@
             // ModelTextBox
             // 
             this.ModelTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ModelTextBox.Location = new System.Drawing.Point(193, 23);
+            this.ModelTextBox.Location = new System.Drawing.Point(193, 19);
             this.ModelTextBox.MaxLength = 255;
             this.ModelTextBox.Name = "ModelTextBox";
             this.ModelTextBox.Size = new System.Drawing.Size(249, 26);
@@ -120,7 +130,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(21, 206);
+            this.label6.Location = new System.Drawing.Point(21, 202);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(175, 17);
             this.label6.TabIndex = 6;
@@ -130,7 +140,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(21, 165);
+            this.label5.Location = new System.Drawing.Point(21, 161);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(176, 17);
             this.label5.TabIndex = 5;
@@ -141,7 +151,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(21, 122);
+            this.label4.Location = new System.Drawing.Point(21, 118);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(173, 17);
             this.label4.TabIndex = 4;
@@ -151,7 +161,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(18, 65);
+            this.label2.Location = new System.Drawing.Point(18, 61);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(176, 17);
             this.label2.TabIndex = 2;
@@ -161,7 +171,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(18, 26);
+            this.label1.Location = new System.Drawing.Point(18, 22);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(177, 17);
             this.label1.TabIndex = 0;
@@ -171,11 +181,11 @@
             // 
             this.SaveButton.BackColor = System.Drawing.Color.MediumSlateBlue;
             this.SaveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.SaveButton.Location = new System.Drawing.Point(263, 260);
+            this.SaveButton.Location = new System.Drawing.Point(193, 228);
             this.SaveButton.Name = "SaveButton";
             this.SaveButton.Size = new System.Drawing.Size(100, 54);
             this.SaveButton.TabIndex = 6;
-            this.SaveButton.Text = "Save";
+            this.SaveButton.Text = "Add";
             this.SaveButton.UseVisualStyleBackColor = false;
             this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
             // 
@@ -184,39 +194,113 @@
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.ForeColor = System.Drawing.Color.IndianRed;
-            this.label8.Location = new System.Drawing.Point(155, 19);
+            this.label8.Location = new System.Drawing.Point(409, 20);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(277, 29);
             this.label8.TabIndex = 0;
             this.label8.Text = "Price Inquiry Creation";
             // 
-            // CloseButton
+            // groupBox2
             // 
-            this.CloseButton.BackColor = System.Drawing.Color.PeachPuff;
-            this.CloseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.CloseButton.ForeColor = System.Drawing.Color.DarkMagenta;
-            this.CloseButton.Location = new System.Drawing.Point(459, 19);
-            this.CloseButton.Name = "CloseButton";
-            this.CloseButton.Size = new System.Drawing.Size(64, 29);
-            this.CloseButton.TabIndex = 0;
-            this.CloseButton.Text = "Close";
-            this.CloseButton.UseVisualStyleBackColor = false;
-            this.CloseButton.Click += new System.EventHandler(this.button1_Click);
+            this.groupBox2.Controls.Add(this.listView1);
+            this.groupBox2.Location = new System.Drawing.Point(535, 131);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(543, 182);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(6, 19);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(513, 150);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Model Number";
+            this.columnHeader1.Width = 120;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Product Description";
+            this.columnHeader2.Width = 120;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Inquiry Form";
+            this.columnHeader3.Width = 100;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Remarks";
+            this.columnHeader4.Width = 80;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Qty";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(233, 67);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(237, 26);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(58, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Price Inquiry No";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(541, 355);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 54);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Done";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // PriceInquiry
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.MediumSeaGreen;
-            this.ClientSize = new System.Drawing.Size(548, 421);
-            this.Controls.Add(this.CloseButton);
+            this.ClientSize = new System.Drawing.Size(1090, 421);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.groupBox1);
             this.Name = "PriceInquiry";
             this.Text = "Price Inquiry";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.PriceInquiry_FormClosed);
+            this.Load += new System.EventHandler(this.PriceInquiry_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -237,6 +321,15 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button SaveButton;
         private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Button CloseButton;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
     }
 }
--- ProductManagementSystem/UI/PriceInquiry.cs
+++ ProductManagementSystem/UI/PriceInquiry.cs
@@ -8,6 +8,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using ProductManagementSystem.LogInUI;
 
 namespace ProductManagementSystem.UI
 {
@@ -17,6 +18,7 @@ namespace ProductManagementSystem.UI
         private SqlConnection con;
         private SqlDataReader rdr;
         ConnectionString cs = new ConnectionString();
+        private int pno;
        
         public PriceInquiry()
         {
@@ -47,59 +49,65 @@ namespace ProductManagementSystem.UI
 
         private void SaveButton_Click(object sender, EventArgs e)
         {
-            if (ModelTextBox.Text == "")
+            if (string.IsNullOrWhiteSpace(ModelTextBox.Text))
             {
                 MessageBox.Show("Please Enter Product Model", "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 ModelTextBox.Focus();
-                return;
+               
             }
-            if (ProDesTextBox.Text == "")
+            else  if (string.IsNullOrWhiteSpace(ProDesTextBox.Text))
             {
                 MessageBox.Show("Please Enter Product Description", "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 ProDesTextBox.Focus();
-                return;
+              
             }
             
-            if (InqFromTextBox.Text == "")
+            else if (string.IsNullOrWhiteSpace(InqFromTextBox.Text ))
             {
                 MessageBox.Show("Please Enter Inquiry From", "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 InqFromTextBox.Focus();
-                return;
+            
             }
-            if (RemarksTextBox.Text == "")
+            else if (string.IsNullOrWhiteSpace(RemarksTextBox.Text ))
             {
                 MessageBox.Show("Please Enter Remarks", "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 RemarksTextBox.Focus();
-                return;
+             
             }
-            if (QtyTextBox.Text == "")
+            else if (string.IsNullOrWhiteSpace(QtyTextBox.Text))
             {
                 MessageBox.Show("Please Enter Product Qty", "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 QtyTextBox.Focus();
-                return;
+
             }
-                                   
-            try
+
+            else
             {
-                con = new SqlConnection(cs.DBConn);
-                con.Open();
-                string query = "insert into PriceInquiry(Model,ProductDescription,InquiryFrom,Remarks,Qty) values(@d1,@d2,@d3,@d4,@d5)";
-                cmd = new SqlCommand(query, con);
-                cmd.Parameters.AddWithValue("@d1", ModelTextBox.Text);
-                cmd.Parameters.AddWithValue("@d2", ProDesTextBox.Text);
-                cmd.Parameters.AddWithValue("@d3", InqFromTextBox.Text);
-                cmd.Parameters.AddWithValue("@d4", RemarksTextBox.Text);
-                cmd.Parameters.AddWithValue("@d5", QtyTextBox.Text);
-                cmd.ExecuteNonQuery();
-                con.Close();
-                MessageBox.Show("Successfully Saved", "Report", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                Reset();
-                SaveButton.Enabled = false;
+                if (listView1.Items.Count < 1)
+                {
+                    ListViewItem l1=new ListViewItem(ModelTextBox.Text);
+                 
+                    l1.SubItems.Add(ProDesTextBox.Text);
+                    l1.SubItems.Add(InqFromTextBox.Text);
+                    l1.SubItems.Add(RemarksTextBox.Text);
+                    l1.SubItems.Add(QtyTextBox.Text);
+                    listView1.Items.Add(l1);
+                    Reset();
+                }
+                else
+                {
+                    ListViewItem l2 = new ListViewItem(ModelTextBox.Text);
+
+                    l2.SubItems.Add(ProDesTextBox.Text);
+                    l2.SubItems.Add(InqFromTextBox.Text);
+                    l2.SubItems.Add(RemarksTextBox.Text);
+                    l2.SubItems.Add(QtyTextBox.Text);
+                    listView1.Items.Add(l2);
+                    Reset();
+                }
             }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
+                
+            
         }
 
         private void ModelTextBox_KeyDown(object sender, KeyEventArgs e)
@@ -163,6 +171,130 @@ namespace ProductManagementSystem.UI
             frm.Show();
         }
 
+        private void PriceInquiry_Load(object sender, EventArgs e)
+        {
+            LoadPNo();
+            if (pno!= 0)
+            {
+                int p = Convert.ToInt32(pno);
+                textBox1.Text = Ordinal(p) + @" Price Inquiry";
+            }
+            else
+            {
+                textBox1.Text = "First Inquiry using this Software";
+            }
+          
+        }
+
+        private void LoadPNo()
+        {
+            try
+            {
+                con = new SqlConnection(cs.DBConn);
+                con.Open();
+
+                string inquiry = "SELECT CONVERT(int, IDENT_CURRENT('PriceInquiry')) as pno";
+                cmd = new SqlCommand(inquiry, con);
+                rdr = cmd.ExecuteReader();
+                if (rdr.Read()&&!rdr.IsDBNull(0))
+                {
+                    //string pno = rdr[0].ToString();
+                    pno = rdr.GetInt32(0);
+                }
+                else
+                {
+                    pno = 0;
+                }
+                con.Close();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+                con.Close();
+                this.Close();
+            }
+        }
+
+        public static string Ordinal(int number)
+        {
+            string suffix = String.Empty;
+            if (number == 11 || number == 12 || number == 13 || number % 100 == 11 || number % 100 == 12 || number % 100 == 13)
+            {
+                suffix = "th";
+            }
+            else if (number == 1 || number % 10 == 1)
+            {
+                suffix = "st";
+            }
+            else if (number == 2 || number % 10 == 2)
+            {
+                suffix = "nd";
+            }
+            else if (number == 3 || number % 10 == 3)
+            {
+                suffix = "rd";
+            }
+            else
+            {
+                suffix = "th";
+            }
+            return String.Format("{0}{1}", number, suffix);
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrWhiteSpace(ModelTextBox.Text) || !String.IsNullOrWhiteSpace(ProDesTextBox.Text) ||
+                !String.IsNullOrWhiteSpace(InqFromTextBox.Text) || !String.IsNullOrWhiteSpace(RemarksTextBox.Text) ||
+                !String.IsNullOrWhiteSpace(QtyTextBox.Text))
+            {
+                MessageBox.Show(@"You For got to add "+"\r \n Add first or clear these");
+            }
+            else if(listView1.Items.Count<1)
+            {
+                MessageBox.Show(@"Nothing to Submit");
+            }
+            else
+            {
+                try
+                {
+                    con = new SqlConnection(cs.DBConn);
+                    con.Open();
+                    SqlTransaction trans = con.BeginTransaction();
+                    string query1 = "insert into PriceInquiry(EntryDate,UserId) values(@d1,@d2)" + "SELECT CONVERT(int, SCOPE_IDENTITY())";
+                    cmd = new SqlCommand(query1, con, trans);
+                    cmd.Parameters.AddWithValue("@d1", DateTime.UtcNow.ToLocalTime());
+                    cmd.Parameters.AddWithValue("@d2", frmLogin.uId2);
+                    int id = (int) cmd.ExecuteScalar();
+                    string query2 = "insert into PriceInquiryDetail(Model,ProductDescription,InquiryFrom,Remarks,Qty,PrInId) values(@d1,@d2,@d3,@d4,@d5,@d6)";
+                    cmd = new SqlCommand(query2, con,trans);
+                    for (int i = 0; i < listView1.Items.Count; i++)
+                    {
+                        cmd.Parameters.Clear();
+                        cmd.Parameters.AddWithValue("@d1", listView1.Items[i].Text);
+                        cmd.Parameters.AddWithValue("@d2", listView1.Items[i].SubItems[1].Text);
+                        cmd.Parameters.AddWithValue("@d3", listView1.Items[i].SubItems[2].Text);
+                        cmd.Parameters.AddWithValue("@d4", listView1.Items[i].SubItems[3].Text);
+                        cmd.Parameters.AddWithValue("@d5", listView1.Items[i].SubItems[4].Text);
+                        cmd.Parameters.AddWithValue("@d6", id);
+                        cmd.ExecuteNonQuery();
+                    }
+                  
+                   cmd.Transaction.Commit();
+                    MessageBox.Show("Successfully Saved", "Report", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    textBox1.Clear();
+                    listView1.Items.Clear();
+                    SaveButton.Enabled = false;
+                    con.Close();
+                    this.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    cmd.Transaction.Rollback();
+                    con.Close();
+                }
+            }
+        }
     }
 
 }
