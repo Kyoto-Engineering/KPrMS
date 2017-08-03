832c1f98531da6071fe7bf6be9afefb38447d3fb
Ikramunnesa <ikraafi@gmail.com>
2017-08-03 09:50:33 +0600

    inquiry feedback button added in reports ui

--- ProductManagementSystem/Reports/ReportsUI.Designer.cs
+++ ProductManagementSystem/Reports/ReportsUI.Designer.cs
@@ -29,15 +29,17 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.PriceInqListButton = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.CloseButton = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.InFeedbackButton = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.InFeedbackButton);
             this.groupBox1.Controls.Add(this.button2);
             this.groupBox1.Controls.Add(this.PriceInqListButton);
             this.groupBox1.Controls.Add(this.button1);
@@ -46,6 +48,19 @@
             this.groupBox1.Size = new System.Drawing.Size(519, 313);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Blue;
+            this.button2.Location = new System.Drawing.Point(322, 27);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(152, 52);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Product List Without Price";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // PriceInqListButton
             // 
@@ -86,18 +101,18 @@
             this.CloseButton.UseVisualStyleBackColor = false;
             this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
             // 
-            // button2
+            // InFeedbackButton
             // 
-            this.button2.BackColor = System.Drawing.Color.DarkSeaGreen;
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.ForeColor = System.Drawing.Color.Blue;
-            this.button2.Location = new System.Drawing.Point(322, 27);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(152, 52);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Product List Without Price";
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.InFeedbackButton.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.InFeedbackButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InFeedbackButton.ForeColor = System.Drawing.Color.Blue;
+            this.InFeedbackButton.Location = new System.Drawing.Point(18, 95);
+            this.InFeedbackButton.Name = "InFeedbackButton";
+            this.InFeedbackButton.Size = new System.Drawing.Size(119, 56);
+            this.InFeedbackButton.TabIndex = 3;
+            this.InFeedbackButton.Text = "Inquiry Feedback";
+            this.InFeedbackButton.UseVisualStyleBackColor = false;
+            this.InFeedbackButton.Click += new System.EventHandler(this.InFeedbackButton_Click);
             // 
             // ReportsUI
             // 
@@ -122,5 +137,6 @@
         private System.Windows.Forms.Button CloseButton;
         private System.Windows.Forms.Button PriceInqListButton;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button InFeedbackButton;
     }
 }
