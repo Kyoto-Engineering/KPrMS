5c3af6d774a1f7e75c141910493aee1059b8fc00
Saqib-Tanmoy <saqib.tanmoy@gmail.com>
2017-08-03 13:20:01 +0600

    Available Within Modified

--- ProductManagementSystem/UI/ReplyForInquiry.cs
+++ ProductManagementSystem/UI/ReplyForInquiry.cs
@@ -640,5 +640,19 @@ namespace ProductManagementSystem.UI
                 e.Handled = true;
             }
         }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+            decimal x;
+            if (ch == (char)Keys.Back)
+            {
+                e.Handled = false;
+            }
+            else if (!char.IsDigit(ch) && ch != '.' || !Decimal.TryParse(textBox2.Text + ch, out x))
+            {
+                e.Handled = true;
+            }
+        }
     }
 }
