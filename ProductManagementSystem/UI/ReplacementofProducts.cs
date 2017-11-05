35d31c3373ee607f563c0938bc65a21895afede1
FarihaHossain <farihafiha@gmail.com>
2017-11-05 15:08:25 +0600

    ReplacementofProducts UI Added

---
+++ ProductManagementSystem/UI/ReplacementofProducts.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ProductManagementSystem.UI
+{
+    public partial class ReplacementofProducts : Form
+    {
+        public ReplacementofProducts()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void ReplacementofProducts_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
